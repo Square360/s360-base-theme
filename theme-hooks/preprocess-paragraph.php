2f9913f03bf0c92a4398300e8c47b6c1dc9e2bcb
Steven Keiser <steven.keiser@square360.com>
2022-09-21 11:14:07 -0400

    cleanup: old paragraph preprocess hook

--- theme-hooks/preprocess-paragraph.php
+++ theme-hooks/preprocess-paragraph.php
@@ -6,22 +6,3 @@
  *
  * Define all paragraph preprocess HOOKs.
  */
-
-use Drupal\Component\Utility\Html;
-
-/**
- * Implements hook_preprocess_paragraph().
- */
-function s360_base_theme_preprocess_paragraph(&$variables) {
-  $elements = $variables['elements'];
-
-  /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
-  $paragraph = $variables['paragraph'];
-  $paragraph_bundle = $paragraph->bundle();
-  $paragraph_view_mode = $elements['#view_mode'];
-
-  $variables['attributes']['class'] = [];
-  $variables['attributes']['class'][] = 'paragraph';
-  $variables['attributes']['class'][] = Html::getClass("paragraph--$paragraph_bundle");
-  $variables['attributes']['class'][] = Html::getClass("paragraph--$paragraph_view_mode");
-}
