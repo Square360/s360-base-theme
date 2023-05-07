c5f6882c1dc9edd2e5a24d725891d77afa25b2fc
Steven Keiser <steven.keiser@square360.com>
2023-05-07 18:58:30 -0400

    feat: updates to base styles

---
+++ assets/src/base/table/table.stories.js
@@ -0,0 +1,10 @@
+import tableTwig from './_table.twig';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Foundations/Tables' };
+
+export const table = () => {
+  return tableTwig();
+}
