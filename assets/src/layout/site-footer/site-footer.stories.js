6c4258dc8935ec2edfc4c4c84b17dd9c985f879e
Steven Keiser <steven.keiser@square360.com>
2023-08-04 11:41:27 -0400

    feat: update site-footer

--- assets/src/layout/site-footer/site-footer.stories.js
+++ assets/src/layout/site-footer/site-footer.stories.js
@@ -1,6 +1,8 @@
 import siteFooterTwig from './site-footer.twig';
 import siteFooterData from './site-footer.yml';
 
+import { footer } from '../../components/navigation/navigation.stories';
+
 /**
  * Storybook Definition.
  */
@@ -9,5 +11,7 @@ export default { title: 'Layout/Site Footer' };
 export const siteFooter = () => {
   return siteFooterTwig({
     ...siteFooterData,
+
+    site_footer_menu: footer(),
   });
 }
