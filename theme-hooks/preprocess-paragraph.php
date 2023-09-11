d0d356af3d4311f8c49f648ce2439f37db9eb08f
Steven Keiser <steven.keiser@square360.com>
2023-09-10 20:35:36 -0400

    feat: update preprocesses

--- theme-hooks/preprocess-paragraph.php
+++ theme-hooks/preprocess-paragraph.php
@@ -8,8 +8,16 @@
  * hook function. e.g. `s360_base_theme_preprocess_paragaraph__[bundle]`
  */
 
+use Drupal\Component\Utility\Html;
+
 /**
  * Implements hook_preprocess_paragraph().
  */
 function s360_base_theme_preprocess_paragraph(&$variables) {
+  /** @var \Drupal\paragraph\Entity\Paragraph $paragraph */
+  $paragraph = $variables['paragraph'];
+  $paragraph_bundle = $paragraph->bundle();
+  $paragraph_bundle_safe = Html::getClass($paragraph_bundle);
+
+  $variables['attributes']['id'] = 'paragraph-' . $paragraph_bundle_safe . '-' . $paragraph->id();
 }
