eeb33bea08559fe68842943711d98dce0fe5cba3
Steven Keiser <steven.keiser@square360.com>
2023-09-11 20:05:46 -0400

    feat: add paragraph-field-links templates

--- assets/src/components/paragraph/link-list/link-list.stories.js
+++ assets/src/components/paragraph/link-list/link-list.stories.js
@@ -2,6 +2,7 @@ import linkListTwig from './link-list.twig';
 import linkListData from './link-list.yml';
 import './link-list.js';
 
+import paragraphFieldLinksTwig from '../../field/paragraph-field-links.twig';
 import linkTwig from '../../../base/link/_link.twig';
 import drupalAttribute from 'drupal-attribute';
 
@@ -12,14 +13,16 @@ export default { title: 'Layout Components/Link List' };
 
 export const linkList = () => {
   let links = [];
+  let paragraphFieldLinks;
 
   linkListData.paragraph_field_links.forEach(link => {
-    links.push(linkTwig({ ...link }));
+    links.push({ content: linkTwig({ ...link })});
+    paragraphFieldLinks = paragraphFieldLinksTwig({ field_items: links });
   });
 
   return linkListTwig({
     ...linkListData,
-    paragraph_field_links: links.join(''),
+    paragraph_field_links: paragraphFieldLinks,
     attributes: new drupalAttribute(Object.entries(linkListData.paragraph_attributes)),
   });
 };
