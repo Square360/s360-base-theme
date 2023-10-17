bb8f519b0f608a7f64be6011535e1680e553ff31
Steven Keiser <steven.keiser@square360.com>
2023-10-17 10:11:48 -0400

    feat: remove alias for s360-toolkit
    we use s360-scss-toolkit

--- assets/config/webpack/resolve/alias.config.ts
+++ assets/config/webpack/resolve/alias.config.ts
@@ -5,6 +5,5 @@ const path = require('path');
 
 export const WEBPACK_RESOLVE_ALIAS = {
   SRC_IMAGES$: path.resolve('src/images'),
-  's360-toolkit': path.resolve(path.join('node_modules', 's360-fundamental-toolkit', 'scss')),
   'core': path.resolve(path.join('src', 'core')),
 };
