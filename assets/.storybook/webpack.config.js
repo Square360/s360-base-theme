dd317c00b4f3ae89c91bfc0304a431463406182d
Steven Keiser <steven.keiser@square360.com>
2023-10-17 10:11:08 -0400

    feat: update storybook files

--- assets/.storybook/webpack.config.js
+++ assets/.storybook/webpack.config.js
@@ -19,7 +19,6 @@ module.exports = async ({ config }) => {
   config.resolve.alias['@ui-paragraph'] = path.resolve(path.join('src', 'components', 'paragraph'));
   config.resolve.alias['@ui-views'] = path.resolve(path.join('src', 'components', 'views'));
 
-  config.resolve.alias['s360-toolkit'] = path.resolve(path.join('node_modules', 's360-fundamental-toolkit', 'scss'));
   config.resolve.alias['core'] = path.resolve(path.join('src', 'core'));
   config.resolve.alias['SRC_IMAGES'] = path.resolve(path.join('src', 'images'));
 
@@ -32,6 +31,18 @@ module.exports = async ({ config }) => {
         options: {
           sourceMap: true,
         },
+      },
+      { loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            parser: 'postcss-scss',
+            plugins: [
+              require('autoprefixer')({
+                grid: true
+              })
+            ]
+          }
+        }
       },
       { loader: 'sass-loader',
         options: {
