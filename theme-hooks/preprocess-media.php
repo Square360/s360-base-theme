20c874aa72b7b40a9b4e2e0dc87b211f1a3257af
Steven Keiser <steven.keiser@square360.com>
2024-01-16 13:18:19 -0500

    update media

--- theme-hooks/preprocess-media.php
+++ theme-hooks/preprocess-media.php
@@ -7,8 +7,6 @@
  * Define all media preprocess HOOKs.
  */
 
-use Drupal\Component\Utility\Html;
-
 /**
  * Implements hook_preprocess_media().
  */
@@ -17,12 +15,4 @@ function s360_base_theme_preprocess_media(&$variables) {
 
   /** @var \Drupal\media\Entity\Media $media */
   $media = $variables['media'];
-
-  $media_bundle = $media->bundle();
-  $media_view_mode = $elements['#view_mode'];
-
-  $variables['attributes']['class'] = [];
-  $variables['attributes']['class'][] = 'media';
-  $variables['attributes']['class'][] = Html::getClass("media--$media_bundle");
-  $variables['attributes']['class'][] = Html::getClass("media--$media_view_mode");
 }
