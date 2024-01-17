1758e364102d7ba7b3d53450e3ec99e2148cf251
Steven Keiser <steven.keiser@square360.com>
2024-01-17 14:55:34 -0500

    remove attributes from remote_video stories

--- ui/src/templates/media/remote-video/component/remote-video.stories.js
+++ ui/src/templates/media/remote-video/component/remote-video.stories.js
@@ -2,8 +2,6 @@ import remoteVideoTwig from './remote-video.twig';
 import remoteVideoData from './remote-video.yml';
 import './remote-video.js';
 
-import drupalAttribute from 'drupal-attribute';
-
 /**
  * Storybook Definition.
  */
@@ -11,5 +9,4 @@ export default { title: 'Media/Remote Video' };
 
 export const remoteVideo = () => remoteVideoTwig({
   ...remoteVideoData,
-  attributes: new drupalAttribute(Object.entries(remoteVideoData.media_attributes)),
 });
