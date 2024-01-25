7a92bb58b3384ecb0bef9d0c0162f19b17c45750
Steven Keiser <steven.keiser@square360.com>
2024-01-25 08:23:00 -0500

    fix setup.php file
    removes style_otions file from the alter function and puts it into rename function

--- setup.php
+++ setup.php
@@ -188,7 +188,6 @@ function get_files_to_alter() {
     's360_base_theme.breakpoints.yml',
     's360_base_theme.info.yml',
     's360_base_theme.libraries.yml',
-    's360_base_theme.style_options.yml',
     's360_base_theme.theme',
   ];
 }
@@ -298,6 +297,7 @@ function get_files_to_rename() {
     's360_base_theme.breakpoints.yml',
     's360_base_theme.info.yml',
     's360_base_theme.libraries.yml',
+    's360_base_theme.style_options.yml',
     's360_base_theme.theme',
   ];
 }
