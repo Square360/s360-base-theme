d5cd9b1ea0f6d1664451ae7a81f476f1bfd11392
Steven Keiser <steven.keiser@square360.com>
2024-02-02 07:45:54 -0500

    add the JS libs for the layout storybook file

--- ui/src/layouts/layouts.stories.js
+++ ui/src/layouts/layouts.stories.js
@@ -1,7 +1,14 @@
 import oneColumTwig from './one-column/_one-column.twig';
+import './one-column/one-column.js';
+
 import twoColumTwig from './two-column/_two-column.twig';
+import './two-column/two-column.js';
+
 import threeColumTwig from './three-column/_three-column.twig';
+import './three-column/three-column.js';
+
 import fourColumTwig from './four-column/_four-column.twig';
+import './four-column/four-column.js';
 
 import layoutData from './layouts.yml';
 import './layouts.js';
