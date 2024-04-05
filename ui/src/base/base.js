1e659b7058d703f585fd93895d2a36c427ac48ef
Steven Keiser <steven.keiser@square360.com>
2024-04-05 08:33:56 -0400

    base updates

--- ui/src/base/base.js
+++ ui/src/base/base.js
@@ -1,20 +1,16 @@
 require.context('SRC_IMAGES', true, /\.(gif|png|jpe?g|svg)$/);
 import './base.scss';
 
-Drupal.behaviors.scrollbarWidth = {
-  attach(context) {
-    const ROOT = document.documentElement;
+document.addEventListener('DOMContentLoaded', () => {
+  const ROOT = document.documentElement;
 
-    const updateScrollbarWidth = () => {
-      ROOT.style.setProperty('--scrollbar-width', `${ window.innerWidth - ROOT.clientWidth }px`);
-    }
-
-    window.addEventListener('load', () => {
-      updateScrollbarWidth();
-    });
-
-    window.addEventListener('resize', () => {
-      updateScrollbarWidth();
-    });
+  const updateScrollbarWidth = () => {
+    ROOT.style.setProperty('--scrollbar-width', `${ window.innerWidth - ROOT.clientWidth }px`);
   }
-}
+
+  window.addEventListener('resize', () => {
+    updateScrollbarWidth();
+  });
+
+  updateScrollbarWidth();
+});
--- ui/src/base/link/link.stories.js
+++ ui/src/base/link/link.stories.js
@@ -1,36 +1,13 @@
 import linkTwig from './_link.twig';
 import linkData from './link.yml';
-import "./_link-storybook-only.scss";
-
-import drupalAttribute from 'drupal-attribute';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Foundations/Links' };
 
-const CTA_STYLE_OPTIONS = {
-  '': 'No CTA Style',
-};
-
-export const link = ({ ctaStyle }) => {
-  let linkAttributes = Object.assign({});
-  let linkClasses = [ctaStyle];
-
-  linkAttributes.class = linkClasses;
-
+export const link = () => {
   return linkTwig({
     ...linkData,
-    attributes: new drupalAttribute(Object.entries(linkAttributes)),
   });
-}
-link.argTypes = {
-  ctaStyle: {
-    name: 'CTA Style',
-    options: Object.keys(CTA_STYLE_OPTIONS),
-    control: {
-      type: 'select',
-      labels: CTA_STYLE_OPTIONS
-    }
-  }
 }
