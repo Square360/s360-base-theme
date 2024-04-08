a38749f2392eac83c32ff4c50532f88a446b2f94
Steven Keiser <steven.keiser@square360.com>
2024-04-08 10:30:27 -0400

    site-layout updates

--- ui/src/templates/site-layout/component/main-menu-toggle/main-menu-toggle.js
+++ ui/src/templates/site-layout/component/main-menu-toggle/main-menu-toggle.js
@@ -1,17 +1,15 @@
 import './main-menu-toggle.scss';
 
-Drupal.behaviors.mainMenuToggle = {
-  attach(context) {
-    const MAIN_MENU_TOGGLE = context.querySelector('[data-js="main-menu-toggle"]');
-    if (!MAIN_MENU_TOGGLE) return;
+document.addEventListener('DOMContentLoaded', () => {
+  const MAIN_MENU_TOGGLE = document.querySelector('[data-js="main-menu-toggle"]');
+  if (!MAIN_MENU_TOGGLE) return;
 
-    MAIN_MENU_TOGGLE.addEventListener('click', () => {
-      if (MAIN_MENU_TOGGLE.getAttribute('aria-expanded') === 'true') {
-        MAIN_MENU_TOGGLE.setAttribute('aria-expanded', 'false');
-      }
-      else {
-        MAIN_MENU_TOGGLE.setAttribute('aria-expanded', 'true');
-      }
-    });
-  }
-}
+  MAIN_MENU_TOGGLE.addEventListener('click', () => {
+    if (MAIN_MENU_TOGGLE.getAttribute('aria-expanded') === 'true') {
+      MAIN_MENU_TOGGLE.setAttribute('aria-expanded', 'false');
+    }
+    else {
+      MAIN_MENU_TOGGLE.setAttribute('aria-expanded', 'true');
+    }
+  });
+});
