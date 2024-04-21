5dc0b1243eb8bc13324f4fb8dc5f751f3ce74ff5
Steven Keiser <steven.keiser@square360.com>
2024-04-20 22:06:05 -0400

    replace hixon with s360_base_theme

--- theme-hooks/preprocess-taxonomy.php
+++ theme-hooks/preprocess-taxonomy.php
@@ -5,7 +5,7 @@
  * preprocess-taxonomy.php
  *
  * Define all taxonomy preprocess HOOKs. Each bundle should provide it's own
- * hook function. e.g. `hixon_preprocess_taxonomy_term__[bundle]`
+ * hook function. e.g. `s360_base_theme_preprocess_taxonomy_term__[bundle]`
  */
 
 /**
