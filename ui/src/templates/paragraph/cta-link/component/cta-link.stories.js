e4bfa8b9ed6d0e52ae91e63581025276bb310d6c
Steven Keiser <steven.keiser@square360.com>
2024-09-09 11:40:14 -0400

    update template paragraph files

--- ui/src/templates/paragraph/cta-link/component/cta-link.stories.js
+++ ui/src/templates/paragraph/cta-link/component/cta-link.stories.js
@@ -4,36 +4,31 @@ import './cta-link.js';
 
 import linkTwig from '@ui-base/link/_link.twig';
 
+import { paragraphFieldCtaLinkStyle } from '.storybook/options.js';
+
 /**
  * Storybook Definition.
  */
 export default { title: 'Layout Components/Cta Link' };
 
-const CTA_LINK_STYLE_OPTIONS = {
-  'primary': 'Primary',
-  'secondary': 'Secondary'
-}
+export const ctaLink = (args) => {
+  let { paragraph_field_link, paragraph_field_cta_link_style } = args ?? '';
 
-export const ctaLink = ({ paragraph_field_cta_link_style }) => {
+  paragraph_field_link = (paragraph_field_link)
+    ? paragraph_field_link
+    : ctaLinkData.paragraph_field_link;
+
+  paragraph_field_cta_link_style = (paragraph_field_cta_link_style)
+    ? paragraph_field_cta_link_style
+    : ctaLinkData.paragraph_field_cta_link_style;
+
   return ctaLinkTwig({
-    ...ctaLinkData,
-    paragraph_field_cta_link_style: (paragraph_field_cta_link_style == undefined)
-      ? ctaLinkData.paragraph_field_cta_link_style
-      : paragraph_field_cta_link_style,
-    paragraph_field_link: linkTwig({ ...ctaLinkData.paragraph_field_link }),
+    ...args,
+    paragraph_field_cta_link_style: paragraph_field_cta_link_style,
+    paragraph_field_link: linkTwig({ link_content: paragraph_field_link }),
   });
-}
-
-ctaLink.argTypes = {
-  paragraph_field_cta_link_style: {
-    name: 'CTA Link Style',
-    options: Object.keys(CTA_LINK_STYLE_OPTIONS),
-    control: {
-      type: 'select',
-      labels: CTA_LINK_STYLE_OPTIONS
-    },
-  }
 };
-ctaLink.args = {
-  paragraph_field_cta_link_style: 'primary',
-}
+ctaLink.argTypes = {
+  paragraph_field_cta_link_style: paragraphFieldCtaLinkStyle,
+};
+ctaLink.args = ctaLinkData;
--- ui/src/templates/paragraph/html-content/component/html-content.stories.js
+++ ui/src/templates/paragraph/html-content/component/html-content.stories.js
@@ -9,9 +9,10 @@ import ckEditorTwig from '@ui-field/ckeditor/component/ckeditor.twig';
  */
 export default { title: 'Layout Components/Html Content' };
 
-export const htmlContent = () => {
+export const htmlContent = (args) => {
   return htmlContentTwig({
     ...htmlContentData,
-    paragraph_field_body: ckEditorTwig({ field_items: [{ content: htmlContentData.paragraph_field_body }] }),
+    paragraph_field_body: ckEditorTwig({ field_items: [{ content: args.paragraph_field_body }] }),
   });
-}
+};
+htmlContent.args = htmlContentData;
