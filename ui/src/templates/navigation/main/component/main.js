e2c6bdc591b53af4b461c74d257840589a11e481
Steven Keiser <steven.keiser@square360.com>
2024-09-09 11:05:59 -0400

    update template navigation files

--- ui/src/templates/navigation/main/component/main.js
+++ ui/src/templates/navigation/main/component/main.js
@@ -1,14 +1,1 @@
 import "./main.scss";
-
-Drupal.behaviors.mainMenu = {
-  attach(context) {
-    let focusableElements;
-    let firstFocusableElement;
-    let lastFocusableElement;
-    let activeDetailsElement;
-    let mouseTimeout;
-
-    const MAIN_MENU = context.querySelector('[data-js="main-menu"]');
-    if (!MAIN_MENU) return;
-  }
-};
