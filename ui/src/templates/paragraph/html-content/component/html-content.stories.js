b5894c534521622b1928ce3bb642e2a63299a852
Steven Keiser <steven.keiser@square360.com>
2024-09-12 08:55:35 -0400

    update paragraph files

--- ui/src/templates/paragraph/html-content/component/html-content.stories.js
+++ ui/src/templates/paragraph/html-content/component/html-content.stories.js
@@ -3,6 +3,7 @@ import htmlContentData from './html-content.yml';
 import './html-content.js';
 
 import ckEditorTwig from '@ui-field/ckeditor/component/ckeditor.twig';
+import { formatParagraphText } from '.storybook/_utils.js';
 
 /**
  * Storybook Definition.
@@ -12,7 +13,7 @@ export default { title: 'Layout Components/Html Content' };
 export const htmlContent = (args) => {
   return htmlContentTwig({
     ...htmlContentData,
-    paragraph_field_body: ckEditorTwig({ field_items: [{ content: args.paragraph_field_body }] }),
+    paragraph_field_body: ckEditorTwig({ field_items: [{ content: formatParagraphText(args.paragraph_field_body) }] }),
   });
 };
 htmlContent.args = htmlContentData;
