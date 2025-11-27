6b8bba251adf06ad02c12ccca0e3efcc1e5a8ee9
Steven Keiser <steven.keiser@square360.com>
2025-11-26 20:50:03 -0500

    feat(): add animators js/scss

---
+++ ui/utils/animators/FadeInSlideLeftAnimator.js
@@ -0,0 +1,12 @@
+import { BaseAnimator } from './BaseAnimator.js';
+
+export class FadeInSlideLeftAnimator extends BaseAnimator {
+  constructor(elements, delay, transitionDuration) {
+    super(elements, delay, transitionDuration, {
+      setAnimatedProperties: (element) => {
+        element.style.setProperty('opacity', 1);
+        element.style.setProperty('translate', '0px 0px');
+      }
+    });
+  }
+}
