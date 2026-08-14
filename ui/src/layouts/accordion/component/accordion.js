fe9d19168c6d7bb9bac2cbd0681d0700d38cd6b6
Steven Keiser <steven.keiser@square360.com>
2026-08-14 14:29:23 -0400

    feat(): update layout—accordion

--- ui/src/layouts/accordion/component/accordion.js
+++ ui/src/layouts/accordion/component/accordion.js
@@ -1,1 +1,14 @@
 import './accordion.scss';
+
+import { Collapsible } from 'utils/Collapsible';
+
+Drupal.behaviors.layoutAccordion = {
+  attach(context) {
+    once('layout-accordion', '.layout--accordion', context).forEach((layout) => {
+      const details = layout.querySelector('.layout__details');
+      if (!details) return;
+
+      new Collapsible(details, '.layout__summary', '.layout__regions');
+    });
+  },
+};
